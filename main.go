42df2096a60f6b9d99b4f4613fbc7889c5ca653b
Anurag Jha <ajha6@dons.usfca.edu>
2019-04-23 00:47:11 -0700

    proj 5 reg testing

--- main.go
+++ main.go
@@ -1,21 +1,20 @@
 package main
 
 import (
-	//"fmt"
-	"./p3"
-	"log"
-	"net/http"
-	"os"
-	//"./p1"
+	"./p5"
 )
 
+//func main() {
+//	router := p3.NewRouter()
+//	if len(os.Args) > 1 {
+//		log.Fatal(http.ListenAndServe(":"+os.Args[1], router))
+//	} else {
+//		log.Fatal(http.ListenAndServe(":6686", router))
+//	}
+//}
+
 func main() {
-	router := p3.NewRouter()
-	if len(os.Args) > 1 {
-		log.Fatal(http.ListenAndServe(":"+os.Args[1], router))
-	} else {
-		log.Fatal(http.ListenAndServe(":6686", router))
-	}
+	p5.Hello()
 }
 
 // func main() {
--- p5/Resgistration.go
+++ p5/Resgistration.go
@@ -1,7 +1,55 @@
 package p5
 
-import "fmt"
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"encoding/pem"
+
+	//"crypto/sha256"
+	"crypto/x509"
+	"encoding/hex"
+	"fmt"
+	"golang.org/x/crypto/sha3"
+	"log"
+)
 
 func Hello() {
-	fmt.Println("Hello")
+
+	//message := "Hello World !!!"
+	//fmt.Println(message)
+
+	priv, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		log.Fatal("Couldnt generate keys - err :", err)
+	}
+	fmt.Println("Private Key is : ", priv.D)
+
+	out := x509.MarshalPKCS1PrivateKey(priv) // out is a byte array
+	//fmt.Println("out : ", out)
+
+	var p = &pem.Block{
+		Type:  "RSA Private Key",
+		Bytes: out,
+	}
+	fmt.Println("pem.Block : ", pem.EncodeToMemory(p))
+
+	//publicKey := &privateKey.PublicKey
+	////fmt.Println("Public Key is : ", publicKey.N)
+	//
+	//hash1 := sha256.New()
+	//encrypted, err := rsa.EncryptOAEP(hash1, rand.Reader, publicKey, []byte(message), []byte("what?"))
+	//if err != nil {
+	//	fmt.Println("Error in Encryption")
+	//}
+	////fmt.Println("encrypted : ",string(encrypted))
+	//
+	//
+	//decrypted, err := rsa.DecryptOAEP(hash1, rand.Reader, privateKey, []byte(encrypted), []byte("what?"))
+	//fmt.Println("Decrypted : ", string(decrypted))
+
+}
+
+func generateHash(mbytes []byte) string {
+	sum := sha3.Sum256(mbytes)
+	return hex.EncodeToString(sum[:])
 }
